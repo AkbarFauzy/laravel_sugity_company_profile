9c934aa324abf25da808212c1391ee46f61ec8b4
meidymahardika <meidymahardika300501@gmail.com>
2024-03-27 08:41:08 +0700

    update lang landing

--- database/migrations/2023_12_31_142628_create_table_services.php
+++ database/migrations/2023_12_31_142628_create_table_services.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('name_ind');
             $table->text('description')->nullable();
+            $table->text('description_ind')->nullable();
             $table->string('img')->nullable();
             $table->timestamps();
         });
